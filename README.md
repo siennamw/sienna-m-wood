c98e87e319029a59db99f986e2070df34e66e8c5
Sienna <sienna.m.wood@gmail.com>
2016-10-26 16:08:16 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sienna-boilerplate-static
+Starter templates, styles, and development environment for simple static web pages using [Nunjucks](https://mozilla.github.io/nunjucks/) templates, [sienna-boilerplate](https://github.com/siennamw/sienna-boilerplate) styles, and [Gulp](http://gulpjs.com/) to build, serve, and deploy.
---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+/* Gulp definitions for Music Crash Courses
+ *
+ * Copyright (C) Sienna M. Wood 2016
+ * 25 June 2016
+ */
+
+var gulp = require('gulp'),
+    nunjucksRender = require('gulp-nunjucks-render'),
+    less = require('gulp-less'),
+    ext_replace = require('gulp-ext-replace'),
+    del = require('del'),
+    gpath = require('path'),
+    webserver = require('gulp-webserver'),
+    rsync = require('gulp-rsync');
+
+
+/* Build
+ * -----------------------------------*/
+gulp.task('build', ['nunjucks', 'ext404', 'copyCSS', 'copyJS', 'copyImages']);
+// also make this the default
+gulp.task('default', ['build']);
+
+/* Nunjucks
+ * -----------------------------------*/
+gulp.task('nunjucks', function () {
+    // Gets .html, .shtml, and .nunjucks files in sources/content
+    return gulp.src('./sources/content/**/*.+(html|shtml|nunjucks)')
+    // Renders template with nunjucks
+        .pipe(nunjucksRender({
+            path: ['./sources/templates']
+        }))
+        // output files in build folder
+        .pipe(gulp.dest('./build')
+    );
+});
+
+/* Change extension on 404 to .shtml
+ * -----------------------------------*/
+gulp.task('ext404', ['nunjucks'], function () {
+    gulp.src('./build/404.html')
+        .pipe(ext_replace('.shtml'))
+        .pipe(gulp.dest('./build'));
+    return del([
+        './build/404.html'
+    ]);
+});
+
+/* Compile LESS to sources
+ * -----------------------------------*/
+gulp.task('less', function () {
+    return gulp.src('./sources/css/*.+(less|css)')
+        .pipe(less({
+            paths: [gpath.join(__dirname, 'less', 'includes')]
+        }))
+        .pipe(gulp.dest('./sources/css')
+    );
+});
+
+/* Copy compiled CSS to build
+ * -----------------------------------*/
+gulp.task('copyCSS', ['less'], function () {
+    gulp.src('./sources/css/styles.css')
+        .pipe(gulp.dest('./build/css')
+    );
+});
+
+/* Copy JS to build
+ * -----------------------------------*/
+gulp.task('copyJS', function () {
+    gulp.src(['./sources/js/scripts.js',
+        './sources/sienna-boilerplate/sienna-boilerplate.js'])
+        .pipe(gulp.dest('./build/js')
+    );
+});
+
+/* Copy Images to build (exclude image-resources directory)
+ * -----------------------------------*/
+gulp.task('copyImages', function () {
+    gulp.src('./sources/images/**/*')
+        .pipe(gulp.dest('./build/images')
+    );
+});
+
+/* Local Server
+ * ---------------------------------- */
+gulp.task('serve', function () {
+    gulp.src('build')
+        .pipe(webserver({
+            port: '9090',
+            livereload: true,
+            directoryListing: {
+                enable: true,
+                path: 'build'
+            },
+            open: true
+        })
+    );
+});
+
+/* Deploy
+ * -----------------------------------*/
+gulp.task('deploy', ['build'], function () {
+    return gulp.src('build/**')
+        .pipe(rsync({
+            root: 'build/',
+            hostname: 'HOSTNAME',
+            destination: 'public_html/SITENAME'
+        })
+    );
+});
